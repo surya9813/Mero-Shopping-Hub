dc11b4d61b9c7f9bb7526ac57bc312fc2428f66a
dipesh <dipeshmahatto@gmail.com>
2025-05-20 00:46:15 +0545

    Modifications

--- database/ecommerce.sql
+++ database/ecommerce.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3308
--- Generation Time: Mar 10, 2025 at 01:59 AM
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2025 at 09:00 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -43,6 +43,32 @@ INSERT INTO `admin` (`id`, `username`, `userpassword`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `oid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `productName` varchar(255) NOT NULL,
+  `productPrice` varchar(255) NOT NULL,
+  `productQuantity` int(11) NOT NULL,
+  `orderStatus` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`oid`, `uid`, `productName`, `productPrice`, `productQuantity`, `orderStatus`) VALUES
+(1, 5, 'Samsung', '22000', 5, 'Pending'),
+(2, 5, 'Samsung', '22000', 3, 'Pending'),
+(3, 5, 'Mac Book', '150000', 5, 'Pending'),
+(4, 5, 'Mac Book', '150000', 1, 'Pending'),
+(5, 5, 'Samsung', '22000', 3, 'Pending');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tblproduct`
 --
 
@@ -59,16 +85,9 @@ CREATE TABLE `tblproduct` (
 --
 
 INSERT INTO `tblproduct` (`id`, `PName`, `PPrice`, `PImage`, `PCategory`) VALUES
-(29, 'Hand Bag', '2600', 'Uploadimage/istockphoto-1271796113-612x612.jpg', 'bag'),
-(30, 'Samsung', '22000', 'Uploadimage/pexels-felix-haumann-1938529-5792169.jpg', 'mobile'),
-(31, 'Lenovo', '100000', 'Uploadimage/laptop lenovo.png', 'laptop'),
-(32, 'Samsung', '17000', 'Uploadimage/pexels-ron-lach-8368973.jpg', 'mobile'),
-(33, 'School Bag', '900', 'Uploadimage/81dy4N3jMZL._AC_SX522_.jpg', 'bag'),
-(34, 'Mac Book', '150000', 'Uploadimage/pexels-shvetsa-5325003.jpg', 'laptop'),
-(35, 'Ladies Bag', '1400', 'Uploadimage/S0a8a3a4e712b44a8a1273905c68a2fad5.jpg_640x640Q90.jpg_.webp', 'bag'),
-(36, 'Hand Bag', '1600', 'Uploadimage/image_file__16597.jpg', 'bag'),
-(37, 'Samsung', '887676', 'Uploadimage/Screenshot (145).png', 'mobile'),
-(38, 'Samsung', '22000', 'Uploadimage/IMG_20230520_170535_952.jpg', 'mobile');
+(39, 'Iphone 11', '68999', 'Uploadimage/4.jpg', 'Iphone'),
+(40, 'Samsung', '215000', 'Uploadimage/5.jpg', 'Samsung'),
+(41, 'Ryte t1', '1500', 'Uploadimage/12.jpg', 'others');
 
 -- --------------------------------------------------------
 
@@ -92,15 +111,10 @@ INSERT INTO `tbluser` (`Id`, `UserName`, `Email`, `Number`, `Password`) VALUES
 (5, 'Dipesh', 'dipesh2000@gmail.com', '9865741230', 'dipesh'),
 (6, 'RAMESH', 'rameshsingh9813@gmail.com', '9812115957', 'ramesh'),
 (7, 'chanda', 'suryanarayanmahato788@gmail.com', '9874563210', '123654'),
-(8, 'Dippesh', 'dipeshmahatto@gmail.com', '9803643491', 'Dipesh'),
-(9, 'Lalbabu Mahato', 'lalbabu@gmail.com', '9874501236', 'lalbabu'),
 (19, 'Rupesh', 'suryanarayrrranmahato788@gmail.com', '9803290797', '00000000'),
 (20, 'admin455', 'suryanaraya14552nmahato788@gmail.com', '0003290797', 'admin123'),
 (21, 'admin14', 'sury145anarayanmahato788@gmail.com', '0003290797', 'surya@1234'),
-(25, 'Ram Mahato', 'ram@gmail.com', '9812002295', 'ram@1234'),
 (26, 'Mohan Mahato', 'mohan142@gmail.com', '9800090797', 'mohan@123'),
-(27, 'Dipeshh', 'dipeshkumarmahato788@gmail.com', '9806721751', 'dipesh@123'),
-(28, 'Dipeshhh', 'suryanarayassdmahato788@gmail.com', '9803200797', 'surya@98032'),
 (29, 'DipeshMahato', 'suryanarayandfghjmahato788@gmail.com', '9803000097', '$2y$10$VJ1yDbN4Er7K16nV2LH25.5O6b7erPQ1bph9dJTrEpNJARNVUboC6');
 
 --
@@ -112,6 +126,12 @@ INSERT INTO `tbluser` (`Id`, `UserName`, `Email`, `Number`, `Password`) VALUES
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`oid`);
 
 --
 -- Indexes for table `tblproduct`
@@ -138,10 +158,16 @@ ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `oid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `tblproduct`
 --
 ALTER TABLE `tblproduct`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `tbluser`
